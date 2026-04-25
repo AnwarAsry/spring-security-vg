ebea07f7c3e803cf8716e2d50664b65b1304e9b2
AnwarAsry <anwareducation03@gmail.com>
2026-04-25 18:27:53 +0200

    Initial commit

---
+++ src/test/java/org/iths/springsecurityvg/SpringSecurityVgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iths.springsecurityvg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityVgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
